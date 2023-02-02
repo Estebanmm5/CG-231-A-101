04dce798aa2f68b617ea291fe151fd29c2d62794
Estebanmm5 <124269560+Estebanmm5@users.noreply.github.com>
2023-02-02 08:57:45 -0500

    Create ordenar.py

---
+++ ordenar.py
@@ -0,0 +1,7 @@
+a=int(input("ingrese un número entero "))
+b=int(input("ingrese un número entero "))
+c=int(input("ingrese un número entero "))
+d=int(input("ingrese un número entero "))
+num=[a, b, c, d]
+num.sort()
+print(num)
